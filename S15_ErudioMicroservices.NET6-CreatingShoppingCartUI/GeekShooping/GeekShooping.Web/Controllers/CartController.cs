45ec6ec531f516b706790acb955cd526446d5188
Edvaldo Henrique <edvaldohenrique.94@gmail.com>
2022-08-20 14:18:03 -0300

    Criando tela de carrinho de compra

---
+++ S15_ErudioMicroservices.NET6-CreatingShoppingCartUI/GeekShooping/GeekShooping.Web/Controllers/CartController.cs
@@ -0,0 +1,57 @@
+﻿using GeekShooping.Web.Models;
+using GeekShooping.Web.Services.IServices;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShooping.Web.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ICartService _cartService;
+        private readonly IProductService _productService;
+
+        public CartController(ICartService cartService, IProductService productService)
+        {
+            _cartService = cartService ?? throw new ArgumentNullException(nameof(cartService));
+            _productService = productService ?? throw new ArgumentNullException(nameof(productService));
+        }
+
+        [Authorize]
+        public async Task<IActionResult> CartIndex()
+        {
+            return View(await FindUserCart());
+        }
+
+        private async Task<CartViewModel> FindUserCart()
+        {
+            var token = await HttpContext.GetTokenAsync("access_token");
+            var userID = User.Claims.Where(U => U.Type == "sub")?.FirstOrDefault()?.Value;
+
+            var response = await _cartService.FindCartByUserId(userID, token);
+
+            if (response?.CartHeader != null)
+            {
+                foreach (var detail in response.CartDetails)
+                {
+                    response.CartHeader.PurchaseAmount += (detail.Product.Price * detail.Count);
+                }
+            }
+
+            return response;
+        }
+
+        public async Task<IActionResult> Remove(int id)
+        {
+            var token = await HttpContext.GetTokenAsync("access_token");
+            var status = await _cartService.RemoveFromCart(id, token);
+
+            if(status)
+            {
+                return RedirectToAction(nameof(CartIndex));
+            }
+
+            return View();
+        }
+    }
+}
---
+++ S15_ErudioMicroservices.NET6-CreatingShoppingCartUI/GeekShooping/GeekShooping.Web/Models/CartHeaderViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace GeekShooping.Web.Models
+{
+    public class CartHeaderViewModel
+    {
+        public long Id { get; set; }
+        public string UserID { get; set; }
+        public string? CouponCode { get; set; }
+        public decimal PurchaseAmount { get; set; }
+    }
+}
