91f9ef041c2b65eedd047055876f7d57c534ed96
Edvaldo Henrique <edvaldohenrique.94@gmail.com>
2022-08-24 10:05:30 -0300

    Integrando checkout ao CartAPI

---
+++ S18_ErudioMicroservices.NET6-IntegratingCheckout/GeekShooping/GeekShopping.CartAPI/Controllers/CartController.cs
@@ -0,0 +1,88 @@
+﻿using GeekShopping.CartAPI.Data.ValueObjects;
+using GeekShopping.CartAPI.Messages;
+using GeekShopping.CartAPI.Repository.Interface;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.CartAPI.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class CartController : ControllerBase
+    {
+        private ICartRepository _cartRepository;
+
+        public CartController(ICartRepository cartRepository)
+        {
+            _cartRepository = cartRepository ?? throw new ArgumentNullException(nameof(cartRepository));
+        }
+
+        [HttpGet("find-cart/{id}")]
+        public async Task<ActionResult<CartVO>> FindByCart(string id)
+        {
+            var cart = await _cartRepository.FindCartByUserId(id);
+
+            if (cart == null)
+                return NotFound();
+            else
+                return Ok(cart);
+        }
+
+        [HttpPost("add-cart")]
+        public async Task<ActionResult<CartVO>> AddCart(CartVO vo)
+        {
+            var cart = await _cartRepository.SaveOrUpdateCart(vo);
+            if (cart == null) return NotFound();
+            return Ok(cart);
+        }
+
+        [HttpPut("update-cart")]
+        public async Task<ActionResult<CartVO>> UpdateCart([FromBody] CartVO cartVO)
+        {
+            if (cartVO == null) return BadRequest();
+            var cart = await _cartRepository.SaveOrUpdateCart(cartVO);
+            return Ok(cart);
+        }
+
+        [HttpDelete("remove-cart/{id}")]
+        public async Task<ActionResult<bool>> RemoveCart(int id)
+        {
+            var status = await _cartRepository.RemoveFromCart(id);
+            if (!status) BadRequest(); 
+            return Ok(status);
+        }
+
+        [HttpPost("apply-coupon")]
+        public async Task<ActionResult<CartVO>> ApplyCoupon(CartVO vo)
+        {
+            var status = await _cartRepository.ApplyCoupon(vo.CartHeader.UserID, vo.CartHeader.CouponCode);
+            if (!status) return NotFound();
+            return Ok(status);
+        }
+
+        [HttpDelete("remove-coupon/{userId}")]
+        public async Task<ActionResult<CartVO>> RemoveCoupon(string userId)
+        {
+            var status = await _cartRepository.RemoveCoupon(userId);
+            if (!status) return NotFound();
+            return Ok(status);
+        }
+
+        [HttpPost("checkout")]
+        public async Task<ActionResult<CheckoutHeaderVO>> Checkout(CheckoutHeaderVO vo)
+        {
+            var cart = await _cartRepository.FindCartByUserId(vo.UserID);
+
+            if (cart == null)
+                return NotFound();
+
+            vo.CartDetails = cart.CartDetails;
+            vo.DateTime = DateTime.Now;
+
+            //TASK RABBITMQ LOGIC COMES HERE!!!
+
+            return Ok(vo);
+        }
+    }
+}
